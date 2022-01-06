2d8253f5ed47c5c878362cc161b4e26f0614617a
dib-a <kirakirito62@gmail.com>
2022-01-06 04:36:56 +0100

    setup script for arch-linux installation

---
+++ config-system.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#vim /etc/locale.gen
+cp src/locale.gen /etc/locale.gen
+locale-gen
+
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc --utc
+
+echo pc > /etc/hostname
+passwd
+#vim /etc/sudoers
+cp src/sudoers /etc/sudoers
+
+systemctl enable dhcpcd
+systemctl enable systemd-timesyncd
+
+#vim /etc/mkinitcpio.conf
+cp src/mkinitcpio.conf /etc/mkinitcpio.conf
+mkinitcpio -P
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch --recheck
+#vim /etc/default/grub
+cp src/grub /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+exit
---
+++ install-system.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+systemctl enable --now systemd-timesyncd
+
+cfdisk /dev/sda
+mkfs.vfat -F 32 -n EFI /dev/sda1
+mkfs.ext4 -L BOOT /dev/sda2
+mkswap -L SWAP /dev/sda4
+cryptsetup -v -y --cipher aes-xts-plain64 --key-size 256 --hash sha256 --iter-time 2000 --use-urandom --verify-passphrase luksFormat /dev/sda3
+cryptsetup open /dev/sda3 SYSTEM
+mkfs.ext4 -L SYSTEM /dev/mapper/SYSTEM
+
+mount -L SYSTEM /mnt
+mkdir /mnt/boot
+mount -L BOOT /mnt/boot
+mkdir /mnt/boot/efi
+mount -L EFI /mnt/boot/efi
+swapon -L SWAP
+
+pacstrap /mnt base base-devel linu linux-firmware dhcpcd grub mkinitcpio efibootmgr git vim sudo
+
+# for wifi uncomment the command below
+# pacstrap /mnt base base-devel linu linux-firmware dhcpcd grub mkinitcpio efibootmgr git vim sudo wpa_supplicant dialog
+
+manjaro-chroot /mnt
