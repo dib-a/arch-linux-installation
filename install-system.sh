68abc7ae50b33e65ebafe04544dc8625980bfeb3
dib-a <96333029+dib-a@users.noreply.github.com>
2022-04-15 07:50:25 +0200

    changed cryptdevice to sda4 and swap to sda3

--- install-system.sh
+++ install-system.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-pacman -Syyu
-
-cfdisk /dev/sda
+fdisk /dev/sda
 mkfs.vfat -F 32 -n EFI /dev/sda1
 mkfs.ext4 -L BOOT /dev/sda2
-mkswap -L SWAP /dev/sda4
-cryptsetup -v -y --cipher aes-xts-plain64 --key-size 256 --hash sha256 --iter-time 2000 --use-urandom --verify-passphrase luksFormat /dev/sda3
-cryptsetup open /dev/sda3 SYSTEM
+mkswap -L SWAP /dev/sda3
+cryptsetup -v -y --cipher aes-xts-plain64 --key-size 256 --hash sha256 --iter-time 2000 --use-urandom --verify-passphrase luksFormat /dev/sda4
+cryptsetup open /dev/sda4 SYSTEM
 mkfs.ext4 -L SYSTEM /dev/mapper/SYSTEM
 
 mount -L SYSTEM /mnt
